ff1f9ddf7994e23517a5b55a2e38891996993330
Syed Bilgrami <bilgrami@gmail.com>
2019-06-17 15:11:43 -0700

    os based env activation

--- shell_scripts/setup-environment.sh
+++ shell_scripts/setup-environment.sh
@@ -1,6 +1,18 @@
-cd ../
 mkdir -p ./.virtualenvs/myproject_env 
 python -m venv ./.virtualenvs/myproject_env 
-./.virtualenvs/myproject_env/bin/activate
+
+if [ "$(uname)" == "Darwin" ]; then
+    echo "You are under Mac OS X platform"        
+    ./.virtualenvs/myproject_env/bin/activate
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo "You are under GNU/Linux platform"
+    ./.virtualenvs/myproject_env/bin/activate
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    echo "You are under 32 bits Windows NT platform" && \
+    ./.virtualenvs/myproject_env/Scripts/activate.bat
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    echo "You are under 64 bits Windows NT platform" && \
+    ./.virtualenvs/myproject_env/Scripts/activate.bat
+fi
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt

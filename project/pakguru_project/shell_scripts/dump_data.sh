21acd057c3aa59824e52844850510bb992dd8c06
Syed A Bilgrami <bilgrami@gmail.com>
2019-07-01 10:02:25 -0700

    data dump with formatted json

--- project/pakguru_project/shell_scripts/dump_data.sh
+++ project/pakguru_project/shell_scripts/dump_data.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-python manage.py dumpdata pakguru_app.Author > ./pakguru_app/fixtures/Author.json
-python manage.py dumpdata pakguru_app.CountryList > ./pakguru_app/fixtures/CountryList.json
-python manage.py dumpdata pakguru_app.LocaleList > ./pakguru_app/fixtures/LocaleList.json
-python manage.py dumpdata pakguru_app.PostCategoryList > ./pakguru_app/fixtures/PostCategoryList.json
-python manage.py dumpdata pakguru_app.ShowChannel > ./pakguru_app/fixtures/ShowChannel.json
-python manage.py dumpdata pakguru_app.ShowSourceFeed > ./pakguru_app/fixtures/ShowSourceFeed.json
-python manage.py dumpdata pakguru_app.Show > ./pakguru_app/fixtures/Show.json
-python manage.py dumpdata pakguru_app.Post > ./pakguru_app/fixtures/Post.json
-python manage.py dumpdata pakguru_app.PostStatistic > ./pakguru_app/fixtures/PostStatistic.json
-
+python manage.py dumpdata pakguru_app.Author | python -m json.tool > ./pakguru_app/fixtures/Author.json
+python manage.py dumpdata pakguru_app.CountryList | python -m json.tool > ./pakguru_app/fixtures/CountryList.json
+python manage.py dumpdata pakguru_app.LocaleList | python -m json.tool > ./pakguru_app/fixtures/LocaleList.json
+python manage.py dumpdata pakguru_app.PostCategoryList | python -m json.tool > ./pakguru_app/fixtures/PostCategoryList.json
+python manage.py dumpdata pakguru_app.ShowChannel | python -m json.tool > ./pakguru_app/fixtures/ShowChannel.json
+python manage.py dumpdata pakguru_app.ShowSourceFeed | python -m json.tool > ./pakguru_app/fixtures/ShowSourceFeed.json
+python manage.py dumpdata pakguru_app.Show | python -m json.tool > ./pakguru_app/fixtures/Show.json
+python manage.py dumpdata pakguru_app.Post | python -m json.tool > ./pakguru_app/fixtures/Post.json
+python manage.py dumpdata pakguru_app.PostStatistic | python -m json.tool > ./pakguru_app/fixtures/PostStatistic.json

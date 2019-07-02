e272d5991d23b5bc24f5f4cb027c1f7bdf527464
Syed A Bilgrami <bilgrami@gmail.com>
2019-07-01 22:25:27 -0700

    admin  page for ShowSourceFeed

--- project/pakguru_project/pakguru_app/admin.py
+++ project/pakguru_project/pakguru_app/admin.py
@@ -8,8 +8,31 @@ admin.site.register(Author)
 admin.site.register(PostCategoryList)
 admin.site.register(LocaleList)
 admin.site.register(Show)
-admin.site.register(ShowSourceFeed)
 admin.site.register(Post)
 admin.site.register(CountryList)
 admin.site.register(ShowChannel)
 admin.site.register(PostStatistic)
+
+
+@admin.register(ShowSourceFeed)
+class ShowSourceFeed(admin.ModelAdmin):
+    list_display = ('feed_name',
+                    'show_name',
+                    'channel', 'is_active', 'feed_source',
+                    'feed_quality', 'priority', 'updated')
+    list_filter = ('feed_source', 'channel', 'is_active', 'country')
+    search_fields = ('feed_name', 'channel__channel_name', 'show_name')
+    date_hierarchy = 'updated'
+    ordering = ('-updated',)
+    list_per_page = 30
+    readonly_fields = ('created', 'effective_date', 'updated',)
+    fields = ('feed_name', 'show_name', 'channel',
+              'playlist_link', 'latest_show_link',
+              'title_example', 'title_search_pattern',
+              'search_api_url', 'search_api_pattern',
+              'expiration_date',
+              'country',
+              'is_active', 'feed_source',
+              'feed_quality', 'priority',
+              'added_by', 'extra_data',)
+    save_on_top = True

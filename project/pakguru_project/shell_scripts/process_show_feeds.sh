785cd66e91e36c72f62ea30282a03d1f44c949db
Syed A Bilgrami <bilgrami@gmail.com>
2019-07-07 14:07:09 -0700

    fixed print instead of echo typo [skip ci]

--- project/pakguru_project/shell_scripts/process_show_feeds.sh
+++ project/pakguru_project/shell_scripts/process_show_feeds.sh
@@ -3,12 +3,12 @@
 set -e
 # True expire and recreate all existing jobs
 # 0 is to process everything
-print 'running harvest_show_feeds_UNT'
+echo 'running harvest_show_feeds_UNT'
 python manage.py harvest_show_feeds_UNT True 0
-print 'running harvest_show_feeds_DOL'
+echo 'running harvest_show_feeds_DOL'
 python manage.py harvest_show_feeds_DOL True 0
-print 'running harvest_show_feeds_VPK'
+echo 'running harvest_show_feeds_VPK'
 python manage.py harvest_show_feeds_VPK True 0
 
-print 'running load_harvested_show_feeds'
+echo 'running load_harvested_show_feeds'
 python manage.py load_harvested_show_feeds -1

bb39a6f51bb58d560741dd0549bb2133810d0799
Syed A Bilgrami <bilgrami@gmail.com>
2019-07-10 05:23:39 -0700

    updated test for contact page, aded test for talkshows and dramaserials

--- project/pakguru_project/frontend_app/tests.py
+++ project/pakguru_project/frontend_app/tests.py
@@ -8,6 +8,7 @@ from django.test import TestCase
 
 # TODO: Configure your database in settings.py and sync before running tests.
 
+
 class ViewTest(TestCase):
     """Tests for the application views."""
 
@@ -26,9 +27,19 @@ class ViewTest(TestCase):
     def test_contact(self):
         """Tests the contact page."""
         response = self.client.get('/contact/')
-        self.assertContains(response, 'Contact', 3, 200)
+        self.assertContains(response, 'Contact', 1, 200)
 
     def test_about(self):
         """Tests the about page."""
         response = self.client.get('/about/')
         self.assertContains(response, 'About', 3, 200)
+
+    def test_talkshows(self):
+        """Tests the talkshows page."""
+        response = self.client.get('/talkshows/')
+        self.assertContains(response, 'Talk Shows', 3, 200)
+
+    def test_dramaserials(self):
+        """Tests the dramaserials page."""
+        response = self.client.get('/dramaserials/')
+        self.assertContains(response, 'Drama Serials', 3, 200)

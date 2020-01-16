b90f130eb4d5e650faef1fde87b5e536e4fced25
Syed Bilgrami <bilgrami@gmail.com>
2020-01-16 00:07:18 -0800

    load data as part of environment setup

--- shell_scripts/setup-environment.sh
+++ shell_scripts/setup-environment.sh
@@ -16,3 +16,16 @@ elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
 fi
 python -m pip install --upgrade pip
 # python -m pip install -r requirements.txt
+echo 'Bringing web container up in detached mode (pointing to dev db)';
+docker-compose up -d;
+echo 'Running any pending migrations';
+docker-compose exec web bash -c 'cd /usr/local/project/pakguru_project && python manage.py migrate';
+echo 'Flushing local db';
+docker-compose exec web bash -c 'cd /usr/local/project/pakguru_project && python manage.py flush --no-input';
+echo 'Running init script';
+docker-compose exec web bash -c 'cd /usr/local/project/pakguru_project && chmod +x ./shell_scripts/init_script.sh && ./shell_scripts//init_script.sh';
+echo 'Running load script on local container';
+docker-compose exec web bash -c 'cd /usr/local/project/pakguru_project && ./shell_scripts/load_data.sh';
+echo 'Bringing local container down';
+docker-compose down;
+echo done;
